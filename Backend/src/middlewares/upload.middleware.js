a0ba5a47aa39aca32ddf8f3ad5a869751441b308
Aditya218-maker <aditya777anand@gmail.com>
2026-07-28 20:40:52 +0530

    feat: Add node-id3 dependency for ID3 tag manipulation and implement storage service for file uploads

--- Backend/src/middlewares/upload.middleware.js
+++ Backend/src/middlewares/upload.middleware.js
@@ -1,14 +1,25 @@
 const multer = require("multer")
 
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage() //Iska matlab hai file ko temporary RAM (Buffer memory) me rakho.
+/**
+ Pehle Disk Storage (Hard drive) me photo save karna padta tha, par memoryStorage() best hota hai agar aap photo ko sidhe Cloudinary ya kisi AI Model / Face Detection API par bhejna chahte ho.
+ */
 
 
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 1024 * 1024 * 10 // 5MB
+        fileSize: 1024 * 1024 * 10 // 10MB
     }
 })
 
 
 module.exports = upload
+
+//Yeh code Multer ka use karke ek File Uploading Tool (Middleware) bana raha hai.
+
+// Jab user aapke backend par koi Image, Video, ya PDF bhejta hai (jaise Face Expression Detection ke liye photo upload karna), to yeh code:
+
+// Us photo ko temporary RAM (Memory) me hold karke rakhta hai.
+
+// File size ko check karta hai ki wo 10MB se badi na ho.
