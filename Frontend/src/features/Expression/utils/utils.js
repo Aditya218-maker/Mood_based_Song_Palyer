ccbd4c4db9be7437123e5c6fc98c091781a732bf
Aditya218-maker <aditya777anand@gmail.com>
2026-07-28 22:45:31 +0530

    feat: Add cloudinary dependency for image uploads and implement storage service; create FaceExpression component for expression detection

--- Frontend/src/features/Expression/utils/utils.js
+++ Frontend/src/features/Expression/utils/utils.js
@@ -3,15 +3,8 @@ import {
     FilesetResolver
 } from "@mediapipe/tasks-vision";
 
-//just remember init function initially setups media pipe library. 
-// NO need to know how it is done, This is called Blackbox programming
 
-/**
- Jab tum kisi function ke parameters mein { landmarkerRef, videoRef, streamRef } likhte ho, toh tum JavaScript ko bol rahe ho:
-"Main is function ko ek poora object bhejunga, tum us object ke andar se yeh teen cheezein nikal kar mujhe direct variable ki tarah de do."
- */
-
-export const init = async ({ landmarkerRef, videoRef, streamRef}) => {
+export const init = async ({ landmarkerRef, videoRef, streamRef }) => {
     const vision = await FilesetResolver.forVisionTasks(
         "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
     );
@@ -34,42 +27,41 @@ export const init = async ({ landmarkerRef, videoRef, streamRef}) => {
     await videoRef.current.play();
 };
 
-export const detect = ({ landmarkerRef, videoRef, setExpression, animationRef }) => {
-    if (!landmarkerRef.current || !videoRef.current || videoRef.current.paused) return;
+export const detect = ({ landmarkerRef, videoRef, setExpression }) => {
+    if (!landmarkerRef.current || !videoRef.current) return;
 
-    if (videoRef.current.readyState >= 2) {
-        const results = landmarkerRef.current.detectForVideo(
-            videoRef.current,
-            performance.now()
-        );
+    const results = landmarkerRef.current.detectForVideo(
+        videoRef.current,
+        performance.now()
+    );
 
-        if (results.faceBlendshapes?.length > 0) {
-            const blendshapes = results.faceBlendshapes[0].categories;
-            const getScore = (name) => blendshapes.find((b) => b.categoryName === name)?.score || 0;
+    if (results.faceBlendshapes?.length > 0) {
+        const blendshapes = results.faceBlendshapes[ 0 ].categories;
 
-            const smileLeft = getScore("mouthSmileLeft");
-            const smileRight = getScore("mouthSmileRight");
-            const jawOpen = getScore("jawOpen");
-            const browUp = getScore("browInnerUp");
-            const frownLeft = getScore("mouthFrownLeft");
-            const frownRight = getScore("mouthFrownRight");
+        const getScore = (name) =>
+            blendshapes.find((b) => b.categoryName === name)?.score || 0;
 
-            let currentExpression = "Neutral";
+        const smileLeft = getScore("mouthSmileLeft");
+        const smileRight = getScore("mouthSmileRight");
+        const jawOpen = getScore("jawOpen");
+        const browUp = getScore("browInnerUp");
+        const frownLeft = getScore("mouthFrownLeft");
+        const frownRight = getScore("mouthFrownRight");
 
-            if (smileLeft > 0.5 && smileRight > 0.5) {
-                currentExpression = "happy 😄";
-            } else if (jawOpen > 0.2 && browUp > 0.2) {
-                currentExpression = "surprised 😲";
-            } else if (frownLeft > 0.2 && frownRight > 0.2) { // ⚠️ Sad limit ko sensible 0.2 kiya taaki glitch na kare
-                currentExpression = "sad 😢";
-            }
+        console.log(getScore("mouthFrownLeft"))
 
-            setExpression(currentExpression);
+        let currentExpression = "Neutral";
+
+        if (smileLeft > 0.5 && smileRight > 0.5) {
+            currentExpression = "happy";
+        } else if (jawOpen > 0.2 && browUp > 0.2) {
+            currentExpression = "surprised";
+        } else if (frownLeft > 0.0001 && frownRight > 0.0001) {
+            currentExpression = "sad";
         }
-    }
-    if (animationRef) {
-        animationRef.current = requestAnimationFrame(() => 
-            detect({ landmarkerRef, videoRef, setExpression, animationRef })
-        );
+
+        setExpression(currentExpression);
+
+        return currentExpression
     }
 };
